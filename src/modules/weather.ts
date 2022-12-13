fb827606df0b7e9da3ad8d18201976b760209b43
BadaHertz52 <lmlwpeace@naver.com>
2022-12-13 21:39:50 +0900

    Make initialState and Modify weather in weather.ts

--- src/modules/weather.ts
+++ src/modules/weather.ts
@@ -1,8 +1,30 @@
 import React from "react";
+import { GET_POSITION, GET_WHEATHER, WeatherAction, WeatherState } from "./types";
 
+//initialState
+const initialState :WeatherState ={
+  nx:null,
+  ny:null,
+  nowWeather:null,
+  hourly:null,
+  weekly: null,
+  nation: null,
+  sunRiseAndSet:null
+};
 
-const weather =()=>{
-  return("weather")
+const weather =(state:WeatherState=initialState, action:WeatherAction):WeatherState=>{
+  switch (action.type) {
+    case GET_POSITION:
+      const nx =60;
+      const ny= 128;
+    return {
+      ...state,
+      nx:nx,
+      ny:ny
+      };
+    default:
+      return state;
+  }
 };
 
 export default weather;
