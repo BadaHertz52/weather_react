ce3d3b838d35bccae151708572e6cfdb0ea336bd
BadaHertz52 <lmlwpeace@naver.com>
2022-12-28 16:31:18 +0900

    [DOC] Make KakaoDocumentType in apiType.ts

--- src/modules/apiType.ts
+++ src/modules/apiType.ts
@@ -195,3 +195,36 @@ export type PmGrade ={
   pm10Grade1h:PmType,
   pm25Grade1h:PmType
 };
+
+export type KakaoDoumentType ={
+  /** H(행정동) 또는 B(법정동) */
+  region_type: string,
+  /** 전체 지역 명칭 */
+  address_name: string,
+  /**
+   * 시도 단위
+   */
+  region_1depth_name: string,
+  /**
+   * 구 단위
+   */
+  region_2depth_name: string,
+  /**
+   * 동 단위
+   */
+  region_3depth_name: string,
+  /**
+   * region_type이 법정동이며, 리 영역인 경우만 존재
+   */
+  region_4depth_name: string,
+  /**
+   * region 코드
+   */
+  code: string,
+  /**
+   * logitude
+   */
+  x: number,
+  /**latitude */
+  y: number
+}
