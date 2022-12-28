57e19bbe47c4590f00587046d35a0ff2aad3754e
BadaHertz52 <lmlwpeace@naver.com>
2022-12-28 23:45:03 +0900

    [DOC.FEAT] Make code that get data from public api and make weather object of which type is Weather in App,tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,58 @@
 import React from 'react';
-import { getApInform, getMidFcast, getSunInform, getSVFcast, getUSNcast, getUSSkyCode } from './modules/api';
-import { SVFBaseTime } from './modules/apiType';
-import { MidLandAreaCode, MidTaAreaCode,  midTaArea, midTaAreaCode, midLand_서울인천경기도 } from './modules/areCodeTyoe';
+import { getApAreaCode, getApInform, getAreaData, getMidFcast, getMidLandAreaCode, getMidTaAreaCode, getSunInform, getSVFcast, getUSNcast, getUSSkyCode } from './modules/api';
+import { MidFcstDay, SVFBaseTime, SVFDay, SVFTime } from './modules/apiType';
+import { MidLandAreaCode, MidTaAreaCode,   ApiAreaCode } from './modules/areaCodeType';
+import { SFGridItem } from './modules/sfGrid';
+import { Day, getSkyType, getWsd, WeatherState } from './modules/weatherTypeAndFn';
+function App () {
+  const changeBaseDate =(day:Date, year:number)=>{
+    const month =day.getMonth()+1;
+    const date = day.getDate();
+    const baseDate = `${year}${month}${date}`;
+    return baseDate
+  };
+  const getYesterDay =(date:number, year:number)=>{
+    const yesterday = new Date( new Date().setDate(date -1));
+    // new Date() 말고 today를 쓰면 today 가 이전 날로 변경되는 오류 발생
+    return changeBaseDate(yesterday , year);
+  };
 
-function App() {
-  const getData =async()=>{
-    const nx ="61";
-    const ny ="128";
+  const changeHourToString =(h:number)=> {
+    if(h<10){
+      return  `0${JSON.stringify(h)}00`
+    }else{
+      return `${JSON.stringify(h)}00`
+    }
+  };
+
+  const getData =async(sfGrid:SFGridItem , longitude:string, latitude:string)=>{
+    const nX:string = typeof sfGrid.nX === "number"? 
+    JSON.stringify(sfGrid.nX)
+    : sfGrid.nX
+    ;
+    const nY: string = typeof sfGrid.nY === "number"? 
+    JSON.stringify(sfGrid.nY)
+    : sfGrid.nY
+    ;
+    const stationName: string[] =(sfGrid.arePt3 !==null && sfGrid.arePt2 !==null )?  [sfGrid.arePt1, sfGrid.arePt2, sfGrid.arePt3] : sfGrid.arePt2!==null? [sfGrid.arePt1, sfGrid.arePt2] :[sfGrid.arePt1];const landRegId: MidLandAreaCode |undefined =getMidLandAreaCode(sfGrid);
+
+    const taRegId:MidTaAreaCode|undefined  = getMidTaAreaCode(sfGrid);
+    const sidoName: ApiAreaCode = getApAreaCode(sfGrid);
     const today = new Date();
     const year = today.getFullYear();
     const hours =today.getHours();
     const minutes =today.getMinutes();
     const date =today.getDate();
     const preHours = hours -1 ;
-    const changeBaseDate =(day:Date)=>{
-      const month =day.getMonth()+1;
-      const date = day.getDate();
-      const baseDate = `${year}${month}${date}`;
-      return baseDate
-    };
-    const getYesterDay =()=>{
-      const yesterday = new Date( new Date().setDate(date -1));
-      // new Date() 말고 today를 쓰면 today 가 이전 날로 변경되는 오류 발생
-      return changeBaseDate(yesterday);
-    };
-    const baseDate_today =changeBaseDate(today);
-    const baseDate_yesterday =getYesterDay();
+
+    const baseDate_today =changeBaseDate(today ,year);
+    const baseDate_yesterday =getYesterDay(date ,year);
     const baseDate_skyCode = minutes < 30 && hours === 0 ? 
                             baseDate_yesterday
                             :
                             baseDate_today ;
     const baseDate_svf = hours < 2 ? baseDate_yesterday :baseDate_today;
+    
     const baseTime_svf:SVFBaseTime=(()=>{
       let time :SVFBaseTime ="0200";
       const svfBaseTime =[2,5,8,11,14 ,17,20,23];
@@ -53,42 +75,151 @@ function App() {
       };
       return time
     })();
-    const changeHourToString =(h:number)=> {
-      if(h<10){
-        return  `0${JSON.stringify(h)}00`
-      }else{
-        return `${JSON.stringify(h)}00`
-      }
-    };
+
     const fcstTime : string = changeHourToString(hours) ;
     const baseTime_skyCode :string = baseDate_skyCode === baseDate_today ?  
     (minutes > 30? fcstTime :changeHourToString(preHours))
     :"2300";
-    const landRegId:MidLandAreaCode = midLand_서울인천경기도 ;
-    const taIndex =midTaArea.indexOf("서울")
-    const taRegId  = midTaAreaCode[taIndex] as MidTaAreaCode;
-    // const skyCode = await getUSSkyCode(nx, ny, baseDate_skyCode,baseTime_skyCode,fcstTime);
-    // const uSNcst = await getUSNcast(nx, ny, baseDate_today, fcstTime);
-    // const sVFcst =await getSVFcast(nx,ny,baseDate_svf,baseTime_svf);
-    // const midFcst =await getMidFcast(landRegId, taRegId,baseDate_today,baseDate_yesterday, hours );
-    // const apGrade = await getApInform("서울",["서울특별시", "강북구","수유2동"]);
-    // const longitude ="126.9706519"; 
-    // const latitude ="37.5841367";
-    // const sunInform =await getSunInform(longitude,latitude,baseDate_today,"Y");
+
+    let timeArry :string[] =[];
+    for (let t = 0; t < 24; t++) {
+      let time ;
+      if(t < 10){
+        time = `0${t}00` ;
+      }else{
+        time = `${t}00`;
+      };
+      timeArry.push(time);  
+    };
+    const baseTimeIndex= timeArry.indexOf(fcstTime);
+    const todayTimeArry = timeArry.slice(baseTimeIndex+1);
+    //get api data
+    const skyCode = await getUSSkyCode(nX, nY, baseDate_skyCode,baseTime_skyCode,fcstTime);
+    const uSNcst = await getUSNcast(nX, nY, baseDate_today, fcstTime);
+    const sVFcst =await getSVFcast(nX,nY,baseDate_svf,baseTime_svf ,timeArry);
+    const midFcst =landRegId !==undefined && taRegId !==undefined? await getMidFcast(landRegId, taRegId,baseDate_today,baseDate_yesterday, hours ): undefined;
+    const apGrade = await getApInform(sidoName,stationName);
+    const sunInform =await getSunInform(longitude,latitude,baseDate_today);
+
+
+
+
+    // state로 변경 
+    if(midFcst !==undefined && sunInform !== null ){
+      const svfDay :Day[] = sVFcst.map((d:SVFDay)=>{
+        const am = d.slice(0,11);
+        const pm =d.slice(12);
+        const getAvg =(arry:number[])=>{
+          const length =arry.length;
+          const sum =arry.reduce((a,b)=>a+b);
+          const avg = sum / length; 
+          return avg 
+        };
+        const amData = {
+          sky : getAvg(am.map((t:SVFTime)=>Number(t.sky))),
+          pop:getAvg(am.map((t:SVFTime)=>Number(t.pop))),
+          pty : getAvg(am.map((t:SVFTime)=> Number(t.pty))),
+        };
+        const pmData = {
+          sky : getAvg(pm.map((t:SVFTime)=>Number(t.sky))),
+          pop:getAvg(pm.map((t:SVFTime)=>Number(t.pop))),
+          pty : getAvg(pm.map((t:SVFTime)=> Number(t.pty))),
+        };
+  
+  
+        const day:Day ={
+          dayslater:sVFcst.indexOf(d), //0-5 (today=0)
+          am:{
+            pop: amData.pop,
+            sky :getSkyType(amData.sky, amData.pty)
+          },
+          pm:{
+            pop: pmData.pop,
+            sky :getSkyType(pmData.sky, pmData.pty)
+          },
+          tmn:Number(d[0].tmn),
+          tmx:Number(d[0].tmx)
+        };
+        return day
+      });
+  
+      const midDay = midFcst?.map((d:MidFcstDay)=>{
+        const day:Day ={
+          dayslater: midFcst.indexOf(d) + 4 ,
+          am: {
+            pop:Number(d.rnStAm),
+            sky: d.wfAm
+          },
+          pm:{
+            pop:Number(d.rnStPm),
+            sky: d.wfPm
+          },
+          tmn: d.taMin,
+          tmx: d.taMax
+        };
+        return day
+      });
+      
+
+      const weather :WeatherState ={
+        nX:nX,
+        nY:nY,
+        nowWeather : {
+          tmp:uSNcst.t1h,
+          sky:skyCode,
+          reh:uSNcst.reh,
+          wind: {
+            vec: getWsd(uSNcst.vec),
+            wsd: uSNcst.wsd
+          },
+          pm10Grade: apGrade.pm10Grade1h,
+          pm25Grade: apGrade.pm25Grade1h
+        },
+        hourly : sVFcst[0].filter((t:SVFTime)=> todayTimeArry.includes(t.fcstTime)).map((t:SVFTime)=>({
+            date:t.fcstDate,
+            hour:t.fcstTime,
+            temp:t.tmp,
+            //강수확률(%)
+            pop:t.pop,
+            //강수량(mm)
+            pcp:t.pcp,
+            wind:{
+              vec :getWsd(t.vec),
+              wsd:t.wsd
+            },
+            reh:t.reh
+          })),
+        weekly:[...svfDay, ...midDay],
+        nation:null,
+        sunRiseAndSet : {
+          sunRise :sunInform.sunrise ,
+          sunSet : sunInform.sunset
+        }
+      };
+
+      console.log("weather", weather);
+    };
+
+
+
+  };
+
   const sucess =async(pos: GeolocationPosition)=>{
     const latitude =JSON.stringify(pos.coords.latitude) ;
     const longitude =JSON.stringify(pos.coords.longitude);
+
     const sfGridItem = await getAreaData(latitude,longitude);
-    console.log("sfGridItem", sfGridItem);
-    return sfGridItem
+    if (sfGridItem !== null){
+      getData(sfGridItem, longitude,latitude);
+    }
   };
-  const getPosition =()=>{
+  const getPosition =async()=>{
     navigator.geolocation.getCurrentPosition((pos)=>sucess(pos),(err)=>{
       console.warn("error",err);
     });
     
   };
-  getData();
+  getPosition();
   return (
     <div className="App">
     </div>
