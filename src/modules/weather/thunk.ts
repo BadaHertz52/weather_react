3eef86e9123e4a82dc5d032b8b22724b48dd553c
BadaHertz52 <lmlwpeace@naver.com>
2023-01-01 03:04:17 +0900

    [FEAT] Make getWeather reducer function in weather/thunk,.ts

---
+++ src/modules/weather/thunk.ts
@@ -0,0 +1,26 @@
+import { ThunkAction } from "redux-thunk";
+import { getWeatherData } from "../api";
+import { PositionState } from "../position/types";
+import { getWeatherAsynce} from "./actions";
+import { WeatherAction, WeatherState } from "./types";
+
+
+export const getWeather =(position:PositionState):ThunkAction<void,WeatherState, unknown,WeatherAction>=>async(dispatch)=>{
+  const {request,success,failure}=getWeatherAsynce;
+  const {longitude, latitude, sfGrid}=position;
+  dispatch(request());
+  if(sfGrid !==null && latitude !==null && longitude !==null){
+    // weatherState 이거나 error message 를 담은 string 
+    const data = await getWeatherData(sfGrid,longitude,latitude);
+    if(typeof data === "string"){
+      const error = new Error (`[Error : weather data]:${data}`);
+      dispatch(failure(error));
+    }else{
+      dispatch(success(data));
+    }
+  }else{
+    const error =new Error(`[Error] Is longitude null? ${longitude ===null} //   Is latitude null? ${latitude ===null} //   Is sfGrid null? ${sfGrid ===null} //  `);
+    dispatch(failure(error)) ; 
+  }
+  
+}
