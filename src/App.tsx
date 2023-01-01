1103a8117032756f0cccbd763225805f801241ab
BadaHertz52 <lmlwpeace@naver.com>
2023-01-01 16:09:09 +0900

    [FEAT] Success to get position data and  dispatch it to rootState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import { useDispatch ,useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
+import {  ThunkDispatch } from 'redux-thunk';
+import { RootState } from './modules';
+import { PositionAction, PositionState } from './modules/position';
+import { getPositionThunk } from './modules/position/thunk';
+import { WeatherAction, WeatherState } from './modules/weather';
+import { getWeatherThunk } from './modules/weather/thunk';
 
 function App () {
-  const dispatch =useDispatch();
-  const weather= useSelector((state) => state);
+  const {weather, position}= useSelector((state:RootState) => state);
+  const positionDispatch =useDispatch<ThunkDispatch<PositionState,unknown,PositionAction>>();
+  const weatherDispatch =useDispatch<ThunkDispatch<WeatherState, unknown,WeatherAction>>();
 
+  useEffect(()=>{
+    console.log("position", position);
+    if(position.state === "none"){
+      positionDispatch(getPositionThunk());
+    }else{
+      console.log("position", position);
+      if(position.sfGrid !==null && weather.state === "none"){
+        weatherDispatch(getWeatherThunk(position));
+      }
+    };
+    console.log("weather", weather);
+  },[position ,weather]);
 
-            
   return (
     <div className="App">
     </div>
