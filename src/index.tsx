e2b81160d098e18e6de218727847b5a0dd0869a6
BadaHertz52 <lmlwpeace@naver.com>
2023-01-10 23:31:30 +0900

    [DOC] Modify  src/index file type to typescript from js to export store.dispatch type and delete reportWebVitals

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import rootReducer, { rootSaga } from './modules/index';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from '@redux-saga/core';
 import { configureStore } from '@reduxjs/toolkit';
 const sagaMiddleware =createSagaMiddleware();
+
 const store = configureStore({
   reducer:rootReducer,
   middleware: [thunk ,sagaMiddleware]
 });
 sagaMiddleware.run(rootSaga);
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export type AppDispatch = typeof store.dispatch;
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
     <App />
@@ -24,4 +27,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
