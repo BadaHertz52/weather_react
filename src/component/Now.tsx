5e2dd171ab4ad857d135021aee43c1d9db1b4f3d
BadaHertz52 <lmlwpeace@naver.com>
2023-01-14 15:11:28 +0900

    [DOC] Make NonePorperty type in Now.tsx

--- src/component/Now.tsx
+++ src/component/Now.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-const Now =()=>{
+import React from 'react';
+import { NowWeather } from '../modules/weather';
 
+type NowProperty ={
+  nowWeather : NowWeather
+};
+const Now =({nowWeather}:NowProperty)=>{
     return(
-      <div className="now"></div>
+      <div className="now">
+        <div className="inner">
+          
+        </div>
+      </div>
     )
 };
 
