826af647b41e061dcbd99a4e89294b99e99b622c
BadaHertz52 <lmlwpeace@naver.com>
2023-01-17 20:47:26 +0900

    [DOC] Make WeekProperty type  and  Make AmPm component , Item compoment and type of these components property  in Week.ts

--- src/component/Week.tsx
+++ src/component/Week.tsx
@@ -1,16 +1,82 @@
 import React from 'react';
-import { Day } from '../modules/weather';
+import { AmPmType, Day } from '../modules/weather';
 import ScrollBtn from './ScrollBtn';
-type ItemPorperty ={
-  day:Day
-}
-const Item =({day}:ItemPorperty)=>{
+import SkyIcon from './SkyIcon';
+type AmPmProperty ={
+  data:AmPmType,
+  am:boolean
+};
+const AmPm =({data ,am}:AmPmProperty)=>{
   return(
-    <li className='week_item'>
-      <div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="weather_inner">
+      <strong className='inner_text left'>
+        <span>오전</span>
+        <span className={`rainfall ${data.pop === 0 ? "none" : ""}`}>
+          {Math.round(data.pop)}%
+        </span>
+      </strong>
+      <SkyIcon
+        skyType={data.sky}
+        day={am}
+      />
+    </div>
+  )
+};
+type ItemPorperty ={
+  item:Day
+};
+const Item =({item}:ItemPorperty)=>{
+  const today = new Date();
+  const month = today.getMonth() +1;
+  const date:string = `${month}.${today.getDate()}`;
+  const week =["일","월", "화","수","목","금","토","일"];
+  const todayDay = today.getDay();
+  const itemDay = todayDay + item.dayslater;
+  const item_day = itemDay >6? week[itemDay - 6] : week[itemDay];
+  const day :string = item.dayslater ===0 ? 
+              "오늘" 
+              :(
+                item.dayslater===1?
+                "내일"
+                :
+                item_day
+              );
+  return(
+    <li className={`item day${item.dayslater}`}>
+      <div className='day_data'>
+        <div className='cell_date'>
+          <span className="date_inner">
+            <strong className='day'>
+              {day}
+            </strong>
+            <span className='date'>
+              {date}
+            </span>
+          </span>
+        </div>
+        <div className='cell_weather'>
+          <AmPm
+            data ={item.am}
+            am={true}
+          />
+          <AmPm
+            data ={item.pm}
+            am={false}
+          />
+        </div>
+        <div className='cell_temperature'>
+          <strong>
+            <span className='lowest'>
+              {item.tmn}
+            </span>
+            <span className='bar'>
+              /
+            </span>
+            <span className='highest'>
+              {item.tmx}
+            </span>
+          </strong>
+        </div>
       </div>
     </li>
   )
@@ -19,15 +85,24 @@ type WeekProperty ={
   week:Day[]
 }
 const Week =({week}:WeekProperty)=>{
+  const twoDays = week.slice(0,2);
   return (
     <div className="week" aria-details='주간 예보'>
       <h2>주간 예보</h2>
       <ul className='box'>
-
+        {twoDays.map((d:Day)=>
+          <Item
+            item = {d}
+          />
+        )}
       </ul>
       <div className="scrollControl">
         <div className="scrollArea">
-
+          {week.map((d:Day)=>
+          <Item
+            item = {d}
+          />
+          )}
         </div>
         {/* <ScrollBtn
 
