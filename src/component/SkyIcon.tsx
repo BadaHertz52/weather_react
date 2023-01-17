08e5649f07b4c356989521a75f7d4e9a578944ef
BadaHertz52 <lmlwpeace@naver.com>
2023-01-17 22:36:20 +0900

    [ERROR, DOC] Fix error that SunnyDayIcon diappear and Move classname that are beind and front to .weahterIcon from .iconWrap

--- src/component/SkyIcon.tsx
+++ src/component/SkyIcon.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { SkyType } from '../modules/weather';
-import {RiSunFill} from 'react-icons/ri';
-import {BsCloudyFill, BsFillCloudRainFill,  BsMoonStarsFill, BsSnow2} from 'react-icons/bs';
+import {BsCloudyFill, BsFillCloudRainFill,  BsMoonStarsFill, BsSnow2 ,BsFillSunFill, BsSunFill} from 'react-icons/bs';
 import {FaCloudShowersHeavy} from 'react-icons/fa';
 type SkyIconProperty ={
   skyType:SkyType,
@@ -16,14 +15,14 @@ type CloudyPorperty = WeatherIconProperty & {
 }
 export const CloudyIcon =({className , rain ,shower}:CloudyPorperty)=>{
   return(
-    <div className='cloudyIcon weatherIcon'>
+    <div className= {`${className} cloudyIcon weatherIcon`}>
       <svg width="0" height="0">
         <linearGradient id="cloudy_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
           <stop  stopColor="#ececec" offset="10%" />
           <stop  stopColor="#c5c5c5" offset="100%" />
         </linearGradient>
       </svg>
-      <div className={`${className} iconWrap`} style={{ fill: "url(#cloudy_gradient)" }} >
+      <div className="iconWrap" style={{ fill: "url(#cloudy_gradient)" }} >
       {
         rain ?
           <BsFillCloudRainFill  />
@@ -43,14 +42,14 @@ export const CloudyIcon =({className , rain ,shower}:CloudyPorperty)=>{
 };
 export const VeryCloudyIcon =({className , rain ,shower}:CloudyPorperty)=>{
   return(
-    <div className='veryCloudyIcon weatherIcon' >
+    <div className={`${className} veryCloudyIcon weatherIcon`} >
       <svg width="0" height="0">
         <linearGradient id="veryCloudy_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
           <stop  stopColor="#c4c2c2" offset="10%" />
           <stop  stopColor="#7a7a7a" offset="100%" />
         </linearGradient>
       </svg>
-      <div className={`${className} iconWrap`} style={{ fill: "url(#veryCloudy_gradient)" }} >
+      <div className="iconWrap" style={{ fill: "url(#veryCloudy_gradient)" }} >
       {rain ?
           <BsFillCloudRainFill />
         :
@@ -68,14 +67,14 @@ export const VeryCloudyIcon =({className , rain ,shower}:CloudyPorperty)=>{
 };
 export const SnowIcon =({className}:WeatherIconProperty)=>{
   return(
-  <div className='snowIcon weatherIcon'>
+  <div className={`snowIcon weatherIcon ${className}`}>
       <svg width="0" height="0">
         <linearGradient id="snow_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
           <stop  stopColor="#a7e7fb" offset="5%" />
           <stop  stopColor="#019cef" offset="100%" />
         </linearGradient>
       </svg>
-      <div className={`${className} iconWrap`} style={{ fill: "url(#snow_gradient)" }} >
+      <div className= "iconWrap"  style={{ fill: "url(#snow_gradient)" }} >
           <BsSnow2/>
       </div>
     </div>
@@ -83,17 +82,17 @@ export const SnowIcon =({className}:WeatherIconProperty)=>{
 };
 export const SunnyDayIcon =()=>{
   return(
-  <div className='sunnyDayIcon weatherIcon'>
-      <svg width="0" height="0">
-        <linearGradient id="sunnyDay_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop  stopColor="#fed712" offset="5%" />
-          <stop  stopColor="#ff9900" offset="100%" />
-        </linearGradient>
-      </svg>
-      <div className='iconWrap' style={{ fill: "url(#sunnyDay_gradient)" }} >
-          <RiSunFill/>
+    <div className='sunnyDayIcon weatherIcon'>
+        <svg width="0" height="0">
+          <linearGradient id="sunnyDay_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop  stopColor="#ffbf3e" offset="5%" />
+            <stop  stopColor="#ff7300" offset="100%" />
+          </linearGradient>
+        </svg>
+        <div className='iconWrap' style={{ fill: "url(#sunnyDay_gradient)" }}  >
+            <BsSunFill/>
+        </div>
       </div>
-    </div>
   )
 };
 
@@ -101,12 +100,12 @@ export const SunnyNightIcon =()=>{
   return(
     <div className='sunnyNightIcon weatherIcon'>
         <svg width="0" height="0">
-          <linearGradient id="sunnyNight_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+          <linearGradient id="sunnyDay_gradient" x1="0%" y1="0%" x2="0%" y2="100%">
             <stop  stopColor="#fff2af" offset="10%" />
             <stop  stopColor="#ffd500" offset="100%" />
           </linearGradient>
         </svg>
-        <div className='iconWrap' style={{ fill: "url(#sunnyNight_gradient)" }}  >
+        <div className='iconWrap' style={{ fill: "url(#sunnyDay_gradient)" }}  >
             <BsMoonStarsFill/>
         </div>
       </div>
@@ -157,7 +156,7 @@ const SkyIcon =({skyType ,day}:SkyIconProperty)=>{
       }
       {skyType ==="구름많음" &&
         <CloudyIcon
-          className='behind'
+          className=''
           rain={false}
           shower={false}
         />
@@ -195,7 +194,7 @@ const SkyIcon =({skyType ,day}:SkyIconProperty)=>{
         skyType ==="흐리고 비/눈") &&
         <div className="overlapping">
           <VeryCloudyIcon 
-            className ={"behind "}
+            className ={"behind"}
             rain={true}
             shower={false}
           />
