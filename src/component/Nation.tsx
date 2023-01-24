fb447fd3c1fdfb2c4a2e0b0e71c5918ade8a81b7
BadaHertz52 <lmlwpeace@naver.com>
2023-01-24 23:15:04 +0900

    [DOC] (Nation.tsx) Make Zone component and its type and html element of which classaname is map

--- src/component/Nation.tsx
+++ src/component/Nation.tsx
@@ -1,8 +1,75 @@
 import React ,{Dispatch, SetStateAction, useState} from 'react';
-import { Area , Day} from '../modules/weather';
+import { Area , AreaInform, AreaNow, Day, SkyType, SunRiseAndSet} from '../modules/weather';
 import {weekArry} from './Week';
 import mapImg from '../assets/map.jpg';
+import SkyIcon from './SkyIcon';
+import { checkDayOrNight } from '../App';
+const now ="now";
+const am ="am";
+const pm ="pm" ;
+type TargetTime = typeof now |typeof am |typeof pm;
 
+type ZoneProperty ={
+  /**
+   * 현재 시간이 낮인지
+   */
+  daytime:boolean,
+  area:Area,
+  targetTime:TargetTime,
+  targetDaylater:number
+};
+const Zone =({daytime,area, targetTime, targetDaylater}:ZoneProperty)=>{
+  const nowData = area.now as AreaNow;
+  const dayData =area.day as Day[];
+  const targetDayData = dayData[targetDaylater]
+  const amData =targetDayData.am ;
+  const pmData =targetDayData.pm ;
+  type ZonWeatherData ={
+    sky:SkyType,
+    temp:number,
+  }
+  const zoneWeatherData =(():ZonWeatherData=>{
+    switch (targetTime) {
+      case now:
+        return {
+          sky:nowData.sky,
+          temp: nowData.temp,
+        }
+      case am:
+        return { sky:amData.sky, 
+                temp: targetDayData.tmn
+                }
+      case pm:
+        return { sky:pmData.sky, 
+                temp: targetDayData.tmx
+              }
+      default:
+        return {
+          sky:nowData.sky,
+          temp: nowData.temp,
+        };
+    }
+  })();
+
+  return(
+    <div 
+    className="zone"
+    >
+      <SkyIcon
+        skyType={zoneWeatherData.sky}
+        daytime={daytime}
+      />
+      <div className="text">
+        <span className='areaName'>
+          {area.areaInform.name}
+        </span>
+        <span className='temp'>
+          {zoneWeatherData.temp}
+        </span>
+      </div>
+  </div>
+  )
+};
 type NWItemProperty ={
   daylater:number,
   today_date:number,
@@ -39,14 +106,12 @@ type NationProperty ={
 };
 const Nation =({nation, todaySunInform}:NationProperty)=>{
   const today = new Date();
+  const hours = today.getHours();
   const todya_date = today.getDate();
   const [targetDaylater, setTargetDaylater] =useState<number>(0);
-  const now ="now";
-  const am ="am";
-  const pm ="pm" ;
-  type TargetTime = typeof now |typeof am |typeof pm;
   const [targetTime, setTargetTime]=useState<TargetTime>(now);
   const weeklyList = nation[0].day?.map((d:Day)=>d.dayslater) as number[];
+  const daytime =checkDayOrNight(hours, todaySunInform)
   return (
     <div className="nation">
       <h2 className='title'>
@@ -62,11 +127,6 @@ const Nation =({nation, todaySunInform}:NationProperty)=>{
         />)}
       </ul>
       <div className="map_wrap">
-        <img
-          className='map_img'
-          src ={mapImg}
-          alt="mapImg"
-        />
         <div className="time_area">
           {targetDaylater===0&&
             <button
@@ -91,7 +151,25 @@ const Nation =({nation, todaySunInform}:NationProperty)=>{
           >
             오후
           </button>
-          
+
+        </div>
+        <div className="map">
+          <img
+            className='map_img'
+            src ={mapImg}
+            alt="mapImg"
+          />
+          {nation.map((area:Area)=>
+            area.day !==null ?
+            <Zone
+              daytime={daytime}
+              area={area}
+              targetTime={targetTime}
+              targetDaylater={targetDaylater}
+            />
+            :
+            "no data"
+          )}
         </div>
       </div>
     </div>
