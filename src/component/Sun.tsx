7133df88b1456526f9ab4b9764ef15b263cd6f58
BadaHertz52 <lmlwpeace@naver.com>
2023-01-28 17:39:24 +0900

    [FEAT,DOC, STYLE] Make function that rotate .moveSun and .bar in .sun  at the right angle by comparing sunrise, sunset time and current time.

--- src/component/Sun.tsx
+++ src/component/Sun.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SunRiseAndSet } from '../modules/weather';
 import {HiArrowSmDown, HiArrowSmUp} from 'react-icons/hi';
+import { changeTwoDigit } from '../modules/api';
+import { CSSProperties } from 'styled-components';
 type SunIconAreaProperty ={
   sunrise:boolean
 }
@@ -58,9 +60,63 @@ type SunProperty ={
   sunRiseAndSet:  SunRiseAndSet[]
 };
 const Sun =({sunRiseAndSet}:SunProperty)=>{
+  const now = new Date();
+  const hours =  Number(now.getHours());
+  const min =  changeTwoDigit(Number(now.getMinutes()));
+  const time = Number(`${hours}${min}`);
   const today ="today";
   const tomorrow ="tomorrow";
   const [targetDay, setTargetDay]= useState<typeof today| typeof tomorrow>(today);
+  const todaySunInfo =sunRiseAndSet[0]; 
+  const [moveSunStyle, setMoveSunStyle]= useState<CSSProperties|undefined>(undefined);
+  const [barStyle, setBarStyle]= useState<CSSProperties>({
+    transform:'rotate(45deg)' 
+  });
+  /**
+   * .moveSun를 회전 시킬 때 기준이 되는 각도 (n번 회전시, n * 기준 각도 만큼 회전함)
+   */
+  const ms_ra = 180 /16;
+  /**
+   * .bar를 회전 시킬 때 기준이 되는 각도 (n번 회전시, n * 기준 각도 만큼 회전함)
+   */ 
+  const bar_ra = (270 - 45) /16;
+
+  const changeRotate =(part:number)=>{
+    setMoveSunStyle({
+      transform:part === 16? 'rotate(181deg)':`rotate(${ms_ra * part}deg)`
+    });
+    setBarStyle({
+      transform: part !==0 ? 
+                `rotate(${bar_ra * part}deg)`
+                :
+                'rotate(45deg)'
+    })
+    
+
+  }
+  useEffect(()=>{
+    if(todaySunInfo.sunRise !==null && 
+      todaySunInfo.sunRise!==undefined &&
+      todaySunInfo.sunSet !==null && 
+      todaySunInfo.sunSet!==undefined 
+      ){
+        
+        const sunrise = Number(`${todaySunInfo.sunRise.slice(1,2)}${todaySunInfo.sunRise.slice(3)}`);
+        const sunset = Number(`${todaySunInfo.sunSet.slice(0,2)}${todaySunInfo.sunSet.slice(3)}`);
+        const term = (sunset - sunrise)/16;
+        if(time >= sunset){
+          changeRotate(16);
+        }else if(time <= sunrise){
+          changeRotate(0)
+        }else{
+          const gap = time - sunrise;
+          const quotient = Math.floor(gap / term);
+          changeRotate(quotient)
+
+        }
+    };
+  },[todaySunInfo]);
+  
   return (
     <div className="sun">
       <h2 className="title">
@@ -83,13 +139,17 @@ const Sun =({sunRiseAndSet}:SunProperty)=>{
       {targetDay === today?
         <div className="sun_panel">
           <div className="sun_chart">
-            <div className="move_sun">
-
+            <div 
+              className="move_sun"
+              style={moveSunStyle}
+            >
             </div>
             <div className="progress_bar">
               <div className="current_bar">
-                <span className='bar'>
-
+                <span 
+                  className='bar'
+                  style={barStyle}
+                >
                 </span>
               </div>
             </div>
